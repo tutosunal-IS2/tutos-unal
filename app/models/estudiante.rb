36c4f4da5184050dbbd448d7e7d4559c2f4a2e06
Michael <mamurciac@unal.edu.co>
2016-09-11 11:07:08 -0500

    Correccion en las validaciones de estudiante

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -1,5 +1,5 @@
 class Estudiante < ApplicationRecord
-	validates :nombre, :presence => {:message => "Usted debe ingresar su nombre"}, :length => {minimum => 3, maximum => 45, :message => "El nombre debe tener al menos 3 caracteres y máximo 45"}
-	validates :plan_de_estudios => :presence => {:message => "Usted debe ingresar su plan de estudios"}, :length => {minimum => 6, maximum => 45, :message => "El nombre debe tener al menos 6 caracteres y máximo 45"}
-	validates :semestre, :numericality => {:only_integer => true, :message => "El dato proporcionado no es válido"}, :presence => {:message => "Por favor mencione en qué semestre está"}
+	validates :nombre, :presence => {:message => "El campo de nombre no puede estar en blanco"}, :length => {minimum => 3, maximum => 45, :message => "El nombre debe tener al menos 3 caracteres y máximo 45"}
+	validates :plan_de_estudios => :presence => {:message => "El campo de plan de estudios no puede estar vacío"}, :length => {minimum => 6, maximum => 45, :message => "El nombre del plan de estudios debe tener al menos 6 caracteres y máximo 45"}
+	validates :semestre, :presence => {:message => "El campo de semestre no puede estar vacío"}, :numericality => {:only_integer => true, :message => "El dato proporcionado no es válido"}
 end
