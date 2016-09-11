e3603d04ccc54e9e5bae76d86126e3938a52c16b
Michael <mamurciac@unal.edu.co>
2016-09-11 10:59:42 -0500

    Correccion en las validaciones de comentario

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,3 +1,3 @@
 class Comentario < ApplicationRecord
-	validates :contenido, :presence => {:message => "EL comentario no puede estar en blanco"}, :length => {minimum => 1, maximum => 45, :message => "El comentario debe tener al menos 1 caracter y máximo 45"}
+	validates :contenido, :presence => {:message => "El comentario no puede estar en blanco"}, :length => {maximum => 300, :message => "El comentario no debe superar los 300 caracteres"}
 end
